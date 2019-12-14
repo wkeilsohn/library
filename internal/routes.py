3b20bbd5bb7edfddf314471fcfbbda46a1daddb8
wkeilsohn <kingwilliam42@gmail.com>
2019-12-14 16:38:52 -0500

    Added dynamic urls and fixed other small bugs

--- internal/routes.py
+++ internal/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from internal import app
 
 from internal.forms import LoginForm, AuthorForm, PublisherForm, BookForm, InventoryForm
--- internal/static/javascript/base.js
+++ internal/static/javascript/base.js
@@ -1,11 +1,14 @@
 function PageNumber(){
 	var num = Math.floor((Math.random() * 100) + 1);
 	num = Math.floor(num / 10);
-	return(num.toString()+ '.jpg');
+	return(num);
 }
 
 function NumString(){
 	var num = PageNumber();
-	num = 'url("static/pictures/' + num + '")';
+	num = num.toString();
+	num = "/static/pictures/".concat(num, ".jpg"); // The answer was just a slash... :(
+	num = decodeURIComponent("url('".concat(num, "')"));
 	return(num);
 }
+

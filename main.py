34521f44a2d5e0505b4cc02c23c1c79c46aed2e0
wkeilsohn <kingwilliam42@gmail.com>
2019-12-15 20:15:34 -0500

    Updated Shell

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-from internal import app
+from internal import app, db
+from internal.models import *
+
+### Remember to Switch this off ###
 app.run(debug=True)
+###             ###             ###
+
+@app.shell_context_processor
+def make_shell_context():
+	return {'db': db,
+			'User': User}

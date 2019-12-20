e9a1a73304067b863a2c7235311852065c3bed1f
wkeilsohn <kingwilliam42@gmail.com>
2019-12-20 15:24:02 -0500

    Updated Routes

--- internal/routes.py
+++ internal/routes.py
@@ -1,21 +1,21 @@
 from flask import render_template, flash, redirect, url_for, request
-from internal import app
+from internal import app, db
 from internal.forms import *
 from flask_login import current_user, login_user, logout_user, login_required
-from internal.models import User
+from internal.models import *
 from werkzeug.urls import url_parse
 
-@app.route("/")
+@app.route("/") # Good...Just add Search/Navigation Features.
 @app.route("/home/")
 @login_required
 def home():
     return render_template('home.html')
 
-@app.route("/about/")
+@app.route("/about/") # Good!... Just fill out the page.
 def about():
 	return render_template('about.html')
 
-@app.route("/login/", methods=['GET', 'POST'])
+@app.route("/login/", methods=['GET', 'POST']) # Good!
 def login():
 	if current_user.is_authenticated:
 		return redirect('/home/')
@@ -33,23 +33,38 @@ def login():
 		return redirect('/home/')
 	return render_template('login.html', form = form)
 
-@app.route("/logout/")
+@app.route("/logout/") # Good!
 def logout():
 	logout_user()
 	return render_template('logout.html')
 
 @app.route("/AddBook/", methods=['GET', 'POST'])
 def books():
-	form1 = BookForm()
-	form2 = AuthorForm()
+	form = AddBookForm()
+	if form.validate_on_submit():
+		booktype = BookType(Plan = form.BookType.Plan.data, ABC = form.BookType.ABC.data, Award = form.BookType.Award.data, \
+			BegRead = form.BookType.BegRead.data, Chapter = form.BookType.Chapter.data, Biography = form.BookType.Biography.data, \
+			Mystery = form.BookType.Mystery.data, Folktales = form.BookType.Folktales.data, Game = form.BookType.Game.data, \
+			Season = form.BookType.Season.data, Code = form.BookType.Holiday.data, Paired = form.BookType.Paired.data, \
+			Poetry = form.BookType.Poetry.data, Professional = form.BookType.Professional.data, Science = form.BookType.Science.data, \
+			SharedRd = form.BookType.SharedRd.data, Sports = form.BookType.Sports.data, Wordless = form.BookType.Wordless.data)
+		db.session.add(booktype)
+		db.session.commit()
+		book = Book(LibraryId = form.LibraryId.data, Title = form.Title.data, FirstAuthor = form.FirstAuthor.data, \
+			SubsequentAuthors = form.SubsequentAuthors.data, PublisherName = form.Publisher.data, PublicationYear = form.PublicationYear.data,\
+			BookTypeId = booktype.id, Fiction = form.Fiction.data)
+		db.session.add(book)
+		db.session.commit()
+		flash('Book Added')
+		return redirect('/AddBook/')
 	return render_template('books.html', form = form)
 
-@app.route("/AddAuthor/", methods=['GET', 'POST'])
+@app.route("/AddAuthor/", methods=['GET', 'POST']) # Good!
 def author():
-	form = AuthorForm()
+	form = AddAuthorForm()
 	if form.validate_on_submit():
 		author = Author(FirstName = form.FirstName.data, MiddleName = form.MiddleName.data, LastName = form.LastName.data)
-		db.session.add(user)
+		db.session.add(author)
 		db.session.commit()
 		flash('Author Added')
 		return redirect('/AddAuthor/')
@@ -59,15 +74,15 @@ def author():
 def item():
 	return render_template('item.html', form = form)
 
-@app.route("/AddPublisher/", methods=['GET', 'POST'])
+@app.route("/AddPublisher/", methods=['GET', 'POST']) # Good!
 def publisher():
-	form = PublisherForm()
+	form = AddPublisherForm()
 	if form.validate_on_submit():
 		publisher = Publisher(Publisher = form.Publisher.data, City = form.City.data, State = form.State.data, Country = form.Country.data)
-		db.session.add(user)
+		db.session.add(publisher)
 		db.session.commit()
 		flash('Publisher Added')
-		return redirect('/AddAuthor/')
+		return redirect('/AddPublisher/')
 	return render_template('publisher.html', form = form)
 
 '''

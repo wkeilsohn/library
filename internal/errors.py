07a2231c2ef2dbc40203a5ecd6a2895ba2dcda09
wkeilsohn <kingwilliam42@gmail.com>
2019-12-21 12:59:34 -0500

    Added Error Handling

--- internal/errors.py
+++ internal/errors.py
@@ -1,12 +1,12 @@
 from flask import render_template
-from internal import app
+from internal import app, db
 
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
-'''
+
 @app.errorhandler(500)
 def not_found_error(error):
+	db.session.rollback()
 	return render_template('500.html'), 500
-'''
